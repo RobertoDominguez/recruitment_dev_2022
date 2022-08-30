901d169ada9fbedb09f7762535f9fdcaf489bf4a
unknown <roberto_123_2000@hotmail.com>
2022-08-29 23:35:08 -0400

    init

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\QueensAttackAPIController;
+use App\Http\Controllers\TestController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/problem-1',[QueensAttackAPIController::class,'queensAttack']);
